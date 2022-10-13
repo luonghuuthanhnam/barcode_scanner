be0eb6296eba23992d04dc2ec9e2cf97ef723303
Luong Nam <luonghuuthanhnam5@gmail.com>
2022-10-13 15:54:18 +0700

    first commit

---
+++ Barcode_Scanner/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Barcode_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_datetime = new System.Windows.Forms.TextBox();
+            this.tb_barcode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_exportall = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.lb_count = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_datetime
+            // 
+            this.tb_datetime.Location = new System.Drawing.Point(88, 104);
+            this.tb_datetime.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_datetime.Name = "tb_datetime";
+            this.tb_datetime.ReadOnly = true;
+            this.tb_datetime.Size = new System.Drawing.Size(193, 23);
+            this.tb_datetime.TabIndex = 0;
+            // 
+            // tb_barcode
+            // 
+            this.tb_barcode.Location = new System.Drawing.Point(88, 68);
+            this.tb_barcode.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_barcode.Name = "tb_barcode";
+            this.tb_barcode.ReadOnly = true;
+            this.tb_barcode.Size = new System.Drawing.Size(193, 23);
+            this.tb_barcode.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Barcode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time";
+            // 
+            // btn_exportall
+            // 
+            this.btn_exportall.Location = new System.Drawing.Point(33, 181);
+            this.btn_exportall.Name = "btn_exportall";
+            this.btn_exportall.Size = new System.Drawing.Size(75, 23);
+            this.btn_exportall.TabIndex = 2;
+            this.btn_exportall.Text = "Export All";
+            this.btn_exportall.UseVisualStyleBackColor = true;
+            this.btn_exportall.Click += new System.EventHandler(this.btn_exportall_Click);
+            // 
+            // btn_export
+            // 
+            this.btn_export.Location = new System.Drawing.Point(33, 142);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(75, 23);
+            this.btn_export.TabIndex = 3;
+            this.btn_export.Text = "Export";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // lb_count
+            // 
+            this.lb_count.AutoSize = true;
+            this.lb_count.Location = new System.Drawing.Point(88, 42);
+            this.lb_count.Name = "lb_count";
+            this.lb_count.Size = new System.Drawing.Size(0, 15);
+            this.lb_count.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 403);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_count);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.btn_exportall);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_barcode);
+            this.Controls.Add(this.tb_datetime);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_datetime;
+        private System.Windows.Forms.TextBox tb_barcode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_exportall;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Label lb_count;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Barcode_Scanner/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Barcode_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tb_barcode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tb_datetime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.label1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.label2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.btn_export.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.btn_exportall.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+        }
+        public Dictionary<string, string> barcode_today = new Dictionary<string, string>();
+        public Array total_barcode_today;
+        public string init_barcode = "";
+        public string current_barcode = "";
+        public string txtfilepath = "";
+        const string SPLITTER = "@splitter#";
+        public delegate void UpdateTextBoxDelegate(string cur_barcode);
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() != "\r")
+            {
+                string cur_char = e.KeyChar.ToString();
+                    
+                init_barcode += cur_char;
+            }
+            else
+            {
+                current_barcode = init_barcode;
+                init_barcode = "";
+                Console.WriteLine(sender);
+                Invoke(new UpdateTextBoxDelegate(InvokeUpdateTextBox), current_barcode);
+
+            }
+        }
+
+        private void playsound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer("warning_sound.wav");
+            player.Play();
+        }
+
+        public void UpdateTextBoxThread()
+        {
+            //Invoke the delegate within the UI thread context
+            Invoke(new UpdateTextBoxDelegate(InvokeUpdateTextBox), current_barcode);
+        }
+
+        public void InvokeUpdateTextBox(string cur_barcode)
+        {
+            DateTime now = DateTime.Now;
+            tb_barcode.Text = cur_barcode;
+            tb_datetime.Text = now.ToString();
+
+
+            if (!barcode_today.ContainsKey(cur_barcode))
+            {
+                barcode_today.Add(cur_barcode, now.ToString());
+                using (StreamWriter w = File.AppendText(txtfilepath))
+                {
+                    string wline = cur_barcode + SPLITTER + now.ToString();
+                    w.WriteLine(wline);
+                }
+            }
+            else
+            {
+                //MessageBox.Show("This barcode already exists!");
+                playsound();
+                //item = container[s];
+            }
+
+            //item.Add("value1");
+        }
+
+        private void load_exist_data(string file_path)
+        {
+            string[] existed_data = System.IO.File.ReadAllText(file_path).Split("\n"); 
+            //existed_data = existed_data.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            foreach (string line in existed_data)
+            {
+                try
+                {
+                    if (line != "")
+                    {
+                        if (line.Split(SPLITTER).Length >= 2)
+                        {
+                            string barcode = line.Split(SPLITTER)[0];
+                            string _datetime = line.Split(SPLITTER)[1];
+                            barcode_today.Add(barcode, _datetime);
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("cannot read existed file");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string today = now.ToString().Split(" ")[0].Replace("/", "_");
+            Thread thread = new Thread(new ThreadStart(UpdateTextBoxThread));
+            string exeFile = (new System.Uri(Assembly.GetEntryAssembly().CodeBase)).AbsolutePath;
+            string exeDir = Path.GetDirectoryName(exeFile);
+            txtfilepath = Path.Combine(exeDir, "scanned_data\\"+today+".txt");
+            bool isExist = File.Exists(txtfilepath);
+            if (isExist == true)
+            {
+                load_exist_data(txtfilepath);
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(txtfilepath));
+            }
+            thread.Start();
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            string exeFile = (new System.Uri(Assembly.GetEntryAssembly().CodeBase)).AbsolutePath;
+            string exeDir = Path.GetDirectoryName(exeFile);
+            string csv_dir = Path.Combine(exeDir, "scanned_data\\exported_csv");
+            if (Directory.Exists(csv_dir) == false)
+            {
+                Directory.CreateDirectory(csv_dir);
+            }
+            export_single_file(txtfilepath);
+        }
+
+        private void export_single_file(string export_file)
+        {
+            string[] existed_data = System.IO.File.ReadAllText(export_file).Split("\n");
+            existed_data = existed_data.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            string[] filenames = export_file.Split("\\");
+            string filename = filenames[filenames.Length - 1];
+            string csv_filepath = export_file.Replace(filename, "exported_csv\\" + filename.Replace(".txt", ".csv"));
+            using (StreamWriter w = new StreamWriter(csv_filepath))
+            {
+            foreach (string line in existed_data)
+            {
+                try
+                {
+                    string barcode = line.Split(SPLITTER)[0].ToString();
+                    string _datetime = line.Split(SPLITTER)[1];
+                    string new_line = "'" + barcode + "," + _datetime;
+                    string wline = new_line;
+                    w.Write(wline);
+                }
+                catch
+                {
+                    MessageBox.Show("cannot export file:" + export_file);
+                }
+            }
+            }
+        }
+
+        private void btn_exportall_Click(object sender, EventArgs e)
+        {
+            string exeFile = (new System.Uri(Assembly.GetEntryAssembly().CodeBase)).AbsolutePath;
+            string exeDir = Path.GetDirectoryName(exeFile);
+            string csv_dir = Path.Combine(exeDir, "scanned_data\\exported_csv");
+            string txt_dir = Path.Combine(exeDir, "scanned_data");
+            if (Directory.Exists(csv_dir) == false)
+            {
+                Directory.CreateDirectory(csv_dir);
+            }
+            DirectoryInfo dinfo = new DirectoryInfo(txt_dir);
+            FileInfo[] Files = dinfo.GetFiles("*.txt");
+            foreach (FileInfo file in Files)
+            {
+                export_single_file(file.FullName);
+            }
+        }
+    }
+}
